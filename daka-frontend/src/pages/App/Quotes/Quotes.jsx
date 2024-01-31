95b60bb8fd9920466ef8c55c56d309534c70dbc1
jsumande <j.sumande@gmail.com>
2024-01-31 16:47:36 +0800

    Added new changes

--- daka-frontend/src/pages/App/Quotes/Quotes.jsx
+++ daka-frontend/src/pages/App/Quotes/Quotes.jsx
@@ -1,13 +1,226 @@
+import { useState } from "react";
+
 import SearchAndFilterComponent from "../../../component/DashboardComponent/SearchAndFilterComponent/SearchAndFilterComponent"
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+
 const Quotes = () => {
+
+  const [age, setAge] = useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <div className="w-full h-full">
 
         <SearchAndFilterComponent/>
 
-        <div className="p-3 w-full flex flex-col items-start justify-start">
-          Test
+        <div className="p-3 w-full flex flex-col items-start justify-start gap-4">
+
+          <div className="w-full flex flex-row items-start justify-between">
+
+              <div className="flex flex-col">
+                <h1 className="text-[#012939] text-4xl font-extrabold">Quotes</h1>
+              </div>
+
+              <div className="flex flex-row gap-3">
+
+                <div className="bg-[#7db00e] px-5 py-1 rounded cursor-pointer text-white">
+                  <h1 className="font-bold text-md">New Quote</h1>
+                </div>
+
+                <div className="border-[1px] border-[#7db00e] px-5 py-1 rounded cursor-pointer text-[#7db00e] ">
+                  <h1 className="font-bold text-md">More Actions</h1>
+                </div>
+
+              </div>
+
+          </div>
+
+          <div className="w-full flex flex-row gap-7">
+
+            <div className="w-8/12 flex flex-col border-[1px] border-gray-300 rounded-sm">
+
+                <div className="w-full flex flex-row items-center justify-between p-3 bg-gray-200 gap-4">
+
+                    <TextField fullWidth id="outlined-basic" label="Search quotes..." variant="outlined" sx={{ bgcolor : 'white' }} />
+
+                    <FormControl fullWidth>
+
+                      <InputLabel id="demo-simple-select-filled-label">Created</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={age}
+                        label="Age"
+                        onChange={handleChange}
+                        sx={{ bgcolor : 'white' }}
+                      >
+                        <MenuItem value="">
+                          <em>All</em>
+                        </MenuItem>
+
+                        <MenuItem value={10}>Last 30 days</MenuItem>
+                        <MenuItem value={20}>This month</MenuItem>
+                        <MenuItem value={30}>Last month</MenuItem>
+                        <MenuItem value={30}>This year</MenuItem>
+                      </Select>
+                      
+                    </FormControl>
+
+                    <FormControl fullWidth>
+
+                      <InputLabel id="demo-simple-select-filled-label">Sort</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Age"
+                        sx={{ bgcolor : 'white' }}
+                      >
+                        <MenuItem value="">
+                          <em>Status</em>
+                        </MenuItem>
+
+                        <MenuItem value={10}>Quotes number</MenuItem>
+                        <MenuItem value={20}>First name</MenuItem>
+                        <MenuItem value={20}>Last name</MenuItem>
+                        <MenuItem value={20}>Star rating</MenuItem>
+
+                      </Select>
+                      
+                    </FormControl>
+
+                    
+                    <FormControl fullWidth>
+
+                      <InputLabel id="demo-simple-select-filled-label">Type</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Age"
+                        sx={{ bgcolor : 'white' }}
+                      >
+                        <MenuItem value="">
+                          <em>All</em>
+                        </MenuItem>
+
+                        <MenuItem value={10}>Draft</MenuItem>
+                        <MenuItem value={20}>Awaiting response</MenuItem>
+                        <MenuItem value={20}>Changes requested</MenuItem>
+                        <MenuItem value={20}>Approved</MenuItem>
+                        <MenuItem value={20}>Converted</MenuItem>
+                        <MenuItem value={20}>Archived</MenuItem>
+                        
+                      </Select>
+                      
+                    </FormControl>
+
+                </div>
+
+                <div className="w-full flex flex-col bg-white items-center justify-center pt-24 pb-96 gap-10">
+
+                    <h1 className="text-[#012939] font-bold text-2xl">Let's create a quote and win new work</h1>
+
+                    <div className="bg-[#7db00e] px-16 py-2 rounded-sm cursor-pointer text-white">
+                      <h1 className="font-extrabold text-lg">Create Quote</h1>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="w-4/12 flex flex-col gap-2">
+
+                <h1 className="text-[#012939] text-2xl font-bold">Overview</h1>
+
+                <div className="w-full max-w-[405px] flex flex-row justify-start gap-3">
+                    
+                    <div className="bg-[#eff0f2] px-9 py-2 flex flex-col items-center justify-center rounded">
+                      <h1 className="text-xl text-gray-700 font-extrabold">0</h1>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h1 className="text-sm text-gray-700 font-bold">Draft</h1>
+                        <h1 className="text-sm text-gray-600 font-sm">Ready to be sent</h1>
+                    </div>
+
+                </div>
+
+                <div className="w-full max-w-[405px] flex flex-row justify-start gap-3">
+                    
+                    <div className="bg-[#fcefde] px-9 py-2 flex flex-col items-center justify-center rounded">
+                      <h1 className="text-xl text-[#886b42] font-extrabold">0</h1>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h1 className="text-sm text-gray-700 font-bold">Awaiting response</h1>
+                        <h1 className="text-sm text-gray-600 font-sm">Sent to the client, but waiting to hear back</h1>
+                    </div>
+
+                </div>
+
+                <div className="w-full max-w-[405px] flex flex-row justify-start gap-3">
+                    
+                    <div className="bg-[#ffe2dc] px-9 py-2 flex flex-col items-center justify-center rounded">
+                      <h1 className="text-xl text-[#54211b] font-extrabold">0</h1>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h1 className="text-sm text-gray-700 font-bold">Changes requested</h1>
+                        <h1 className="text-sm text-gray-600 font-sm">Client has requested changes</h1>
+                    </div>
+
+                </div>
+
+                <div className="w-full max-w-[405px] flex flex-row justify-start  gap-3">
+                    
+                    <div className="bg-[#ebf4d8] px-9 py-2 flex flex-col items-center justify-center rounded">
+                      <h1 className="text-xl text-[#526a2d] font-extrabold">0</h1>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h1 className="text-sm text-gray-700 font-bold">Approved</h1>
+                        <h1 className="text-sm text-gray-600 font-sm">Ready to be converted to jobs</h1>
+                    </div>
+
+                </div>
+
+                <div className="w-full max-w-[405px] flex flex-row justify-start  gap-3">
+                    
+                    <div className="bg-[#e2f2ef] px-9 py-2 flex flex-col items-center justify-center rounded">
+                      <h1 className="text-xl text-[#4e7467] font-extrabold">0</h1>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h1 className="text-sm text-gray-700 font-bold">Converted</h1>
+                        <h1 className="text-sm text-gray-600 font-sm">Converted to jobs</h1>
+                    </div>
+
+                </div>
+
+                <div className="w-full max-w-[405px] flex flex-row justify-start  gap-3">
+                    
+                    <div className="bg-[#eff0f2] px-9 py-2 flex flex-col items-center justify-center rounded">
+                      <h1 className="text-xl text-gray-700 font-extrabold">0</h1>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h1 className="text-sm text-gray-700 font-bold">Archived</h1>
+                        <h1 className="text-sm text-gray-600 font-sm">Not converted to jobs</h1>
+                    </div>
+
+                </div>
+
+            </div>
+
+          </div>
+
         </div>
 
 
--- daka-frontend/src/pages/App/Reports/Reports.jsx
+++ daka-frontend/src/pages/App/Reports/Reports.jsx
@@ -6,10 +6,182 @@ const Reports = () => {
 
         <SearchAndFilterComponent text={"Reports"}/>
 
-        <div className="p-3 w-full flex flex-col items-start justify-start">
-          Test
-        </div>
+        <div className="p-3 w-full h-full flex flex-col flex-nowrap items-start justify-start gap-4">
 
+            <div className="w-full flex flex-row items-start justify-between">
+
+                <div className="flex flex-col">
+                  <h1 className="text-[#012939] text-4xl font-extrabold">Reports</h1>
+                </div>
+
+            </div>
+
+            <div className="w-full flex flex-row justify-start gap-10">
+
+              <div className="w-full flex flex-col border-[1px] border-gray-300 rounded-sm">
+
+                <div className="w-full flex flex-col p-3 bg-gray-200">
+                    <h1 className="text-[#012939] font-bold text-lg">Financial reports</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Projected income</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Projected income from invoices awaiting payment</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Transaction list</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">All transactions from invoices, payments & deposits</h1>
+                </div>
+                
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Invoices</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">New invoices report with additional client data</h1>
+                </div>
+                
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Taxation</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Tax totals, total awaiting collection, and total by tax rate</h1>
+                </div>
+                
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Aged receivables</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Invoices that are late by 30, 60, and 90+ days</h1>
+                </div>
+                
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Bad debt</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">All invoices marked as bad debt</h1>
+                </div>
+                
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Client balance summary</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Full list of customer account balances</h1>
+                </div>
+
+              </div>
+
+              <div className="w-full flex flex-col border-[1px] border-gray-300 rounded-sm">
+
+                <div className="w-full flex flex-col p-3 bg-gray-200">
+                    <h1 className="text-[#012939] font-bold text-lg">Work reports</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Visits</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Detailed list of past and upcoming visits</h1>
+                </div>
+                
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">One-off jobs</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">New one-off job report with additional client and job data</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">One-off jobs (Legacy)</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Detailed list of all one-off jobs</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Recurring jobs</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">New recurring jobs report with additional client data</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Recurring jobs (Legacy)</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Detailed list of all recurring jobs</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Requests</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">New requests and assessments report</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Quotes</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">New quotes report with additional client data</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Quotes created (Legacy)</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Detailed list of all created quotes</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Quotes converted (Legacy)</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Detailed list of quotes that have been converted into jobs</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Products & Services</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Full usage of products & services on quotes, jobs, and invoices</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Waypoints</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">Full list of GPS waypoints logged</h1>
+                </div>
+
+                <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer hover:bg-gray-50">
+                    <h1 className="text-[#012939] font-bold text-md">Timesheets</h1>
+                    <h1 className="text-gray-600 font-sm text-sm">All time tracked for the team</h1>
+                </div>
+              
+              </div>
+
+              <div className="w-full flex flex-col gap-10">
+
+                <div className="w-full flex flex-col border-[1px] border-gray-300 rounded-sm">
+
+                  <div className="w-full flex flex-col p-3 bg-gray-200">
+                      <h1 className="text-[#012939] font-bold text-lg">Client reports</h1>
+                  </div>
+
+                  <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                      <h1 className="text-[#012939] font-bold text-md">Client communications</h1>
+                      <h1 className="text-gray-600 font-sm text-sm">All email messages sent through Jobber</h1>
+                  </div>
+
+                  <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                      <h1 className="text-[#012939] font-bold text-md">Job follow-up emails</h1>
+                      <h1 className="text-gray-600 font-sm text-sm">All job follow-up emails sent to clients</h1>
+                  </div>
+
+                  <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                      <h1 className="text-[#012939] font-bold text-md">Client contact info</h1>
+                      <h1 className="text-gray-600 font-sm text-sm">All clients and their contact info</h1>
+                  </div>
+
+                  <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer border-b-[0.5px] border-gray-300 hover:bg-gray-50">
+                      <h1 className="text-[#012939] font-bold text-md">Property list</h1>
+                      <h1 className="text-gray-600 font-sm text-sm">All properties and their details</h1>
+                  </div>
+
+                  <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer hover:bg-gray-50">
+                      <h1 className="text-[#012939] font-bold text-md">Client re-engagement</h1>
+                      <h1 className="text-gray-600 font-sm text-sm">Detailed list of clients that haven't had a closed job in the past 12 months</h1>
+                  </div>
+
+                </div>
+
+                <div className="w-full flex flex-col border-[1px] border-gray-300 rounded-sm">
+
+                  <div className="w-full flex flex-col p-3 bg-gray-200">
+                      <h1 className="text-[#012939] font-bold text-lg">Expense reports</h1>
+                  </div>
+
+                  <div className="w-full flex flex-col pl-3 py-2 bg-white cursor-pointer hover:bg-gray-50">
+                      <h1 className="text-[#012939] font-bold text-md">Expenses</h1>
+                      <h1 className="text-gray-600 font-sm text-sm">All tracked expenses and their details</h1>
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+        </div>
 
     </div>
   )
